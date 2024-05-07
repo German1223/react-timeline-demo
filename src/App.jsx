a0236849dba889ebe1e822ea0b5677f3c8cd836c
Herman S <herman.stepanov.lineardev@gmail.com>
2024-05-07 22:49:12 +0300

    integrate library

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-// import { ConnectWidget } from "reactjs-storage-timeline";
+
+import { StorageTimelineWidget } from "reactjs-storage-timeline";
 import "./App.css";
-import ConnectWidget from "./components/ConnectWidget/ConnectWidget";
 
 function App() {
     return (
         <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh"}}>
-            {/* <ConnectWidget/> */}
-            <ConnectWidget className={'main-widget'}/>
+            <StorageTimelineWidget/>
         </div>
     );
 }
